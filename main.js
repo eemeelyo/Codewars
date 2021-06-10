c26c9139ddbecc646f710dbb55ccc473c337a32d
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-06-10 13:18:07 +0100

    Squares of square

--- main.js
+++ main.js
@@ -120,11 +120,11 @@ function countPositivesSumNegatives(input) {
   let positive = 0;
   let negative = 0;
   if (!input || !input.length) {
-  return [];
-    }
-  
-  for (let i = 0; i < input.length; i++){
-    if (input[i] < 0){
+    return [];
+  }
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] < 0) {
       negative += input[i];
     } else if (input[i] > 0) {
       positive++;
@@ -133,170 +133,191 @@ function countPositivesSumNegatives(input) {
     }
   }
 
-    console.log([positive, negative])
-      return([positive, negative]);
+  console.log([positive, negative]);
+  return [positive, negative];
   // }-----------------------------------------------------------------------------------------------------------------------
-//Heads and Legs
-function animals(heads, legs) {
-  var x = (4 * heads - legs) / 2;
-  var y = heads - x;
-  if (x < 0 || y < 0) {
-    return "No solutions";
-  }
-  if (x === Math.round(Math.abs(x))) {
-    return [x, heads - x];
-  } else {
-    return "No solutions";
-  }
-}
-//Match My Husband
-function match(usefulness, months) {
-  var sum = usefulness.reduce(function (a, b) {
-    return a + b;
-  }, 0);
-  var grvt = 100 * Math.pow(Math.E, -0.15 * months);
-  if (sum >= Math.round(grvt, 3)) {
-    return "Match!";
-  } else {
-    return "No match!";
-  }
-}
-
-//--------------------------------------------------------------------------------------
-// Training JS #15: Methods of Number object--toFixed(), toExponential() and toPrecision()
-
-function howManySmaller(arr, n) {
-  //coding here..
-  var low = [];
-  let high = [];
-  console.log(n);
-
-  for (let i = 0; i < arr.length; i++) {
-    let arrFixed = arr[i].toFixed(2);
-    if (arrFixed < n) {
-      low.push(arrFixed);
-    } else {
-      high.push(arrFixed);
+  //Heads and Legs
+  function animals(heads, legs) {
+    var x = (4 * heads - legs) / 2;
+    var y = heads - x;
+    if (x < 0 || y < 0) {
+      return "No solutions";
     }
-    console.log(low);
+    if (x === Math.round(Math.abs(x))) {
+      return [x, heads - x];
+    } else {
+      return "No solutions";
+    }
   }
-  return low.length;
+  //Match My Husband
+  function match(usefulness, months) {
+    var sum = usefulness.reduce(function (a, b) {
+      return a + b;
+    }, 0);
+    var grvt = 100 * Math.pow(Math.E, -0.15 * months);
+    if (sum >= Math.round(grvt, 3)) {
+      return "Match!";
+    } else {
+      return "No match!";
+    }
+  }
+
+  //--------------------------------------------------------------------------------------
+  // Training JS #15: Methods of Number object--toFixed(), toExponential() and toPrecision()
+
+  function howManySmaller(arr, n) {
+    //coding here..
+    var low = [];
+    let high = [];
+    console.log(n);
+
+    for (let i = 0; i < arr.length; i++) {
+      let arrFixed = arr[i].toFixed(2);
+      if (arrFixed < n) {
+        low.push(arrFixed);
+      } else {
+        high.push(arrFixed);
+      }
+      console.log(low);
+    }
+    return low.length;
+  }
+
+  //---------------------------------------------------------------------------------------
+  // If you can't sleep, just count sheep!!
+  var countSheep = function (num) {
+    //your code here
+    //       let count = i + 1;
+    let sheepCount = "";
+    //   if (num > 0){
+    for (let i = 0; i < num; i++) {
+      sheepCount += i + 1 + " sheep...";
+    }
+    return sheepCount;
+    //   }
+  };
+  //----------------------------------------------------------------------------------------
+  // Multiplication table for number
+  function multiTable(number) {
+    // good luck
+    let timesTable = "";
+    for (let i = 1; i <= 10; i++) {
+      //     console.log(number * i);
+      if (i === 10) {
+        timesTable += i + " * " + number + " = " + number * i;
+      } else {
+        timesTable += i + " * " + number + " = " + number * i + "\n";
+      }
+    }
+    return timesTable;
+  }
+  //----------------------------------------------------------------------------------------
+  // Remove Exclamation
+  return s.replace(/!/g, "");
+  //----------------------------------------------------------------------------------------
+  //Returning Strings
+  function greet(name) {
+    return `Hello, ${name} how are you doing today?`;
+  }
+  //----------------------------------------------------------------------------------------
+  //Sentence Smash
+  // Smash Words
+  function smash(words) {
+    //     "use strict";
+    let rToS = [...words].join(" ");
+    console.log(rToS);
+    return rToS;
+  }
+  //----------------------------------------------------------------------------------------
+  //Training JS #16: Methods of String object--slice(), substring() and substr()
+  function cutIt(arr) {
+    //coding here...
+    let shortestLength = Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+      //     console.log(arr[i]);
+      if (arr[i].length < shortestLength) {
+        shortestLength = arr[i].length;
+      }
+      console.log(shortestLength);
+    }
+    return arr.map((string) => string.slice(0, shortestLength));
+  }
+  //---------------------------------------------------------------------------------------
+  // Penguin Olympics: Swimming Race Disaster
+
+  function calculateWinners(snapshot, penguins) {
+    // TODO: solve kata
+    // RETURN "GOLD: <name-1>, SILVER: <name-2>, BRONZE: <name-3>"
+    // Top three penguins
+    // Snapshot is String of lanes
+    // Each has one Penguin
+    // Lanes are same length
+    // Penguis are names of penguins in order of lanes
+    // Penguins can be "p" or "P"
+    // Split snapshot into distance remaining.
+    // Count the value of the distance remaining in each lane.
+    // When counting "-" + 1 and "~" + 2
+    // Label penguins
+    // Order penguins with the least difficult route
+    // Return the winning panguins
+
+    const lanesArray = snapshot
+      .split("|")
+      .filter((lane) => lane.toLowerCase().includes("p"));
+    const remainingWaterArray = lanesArray.map((lane) => {
+      const penguinIndex = lane.toLowerCase().indexOf("p");
+      const remainingWater = lane.slice(penguinIndex + 1);
+      return remainingWater;
+    });
+    const remainingTimeArray = remainingWaterArray.map((lane) =>
+      calculateTimeRemaining(lane)
+    );
+    const penguinsAndTimesArray = remainingTimeArray.map((time, index) => {
+      return { time: time, penguin: penguins[index] };
+    });
+    const sortedWinnersArray = penguinsAndTimesArray.sort(
+      (a, b) => a.time - b.time
+    );
+    console.log(sortedWinnersArray);
+
+    return `GOLD: ${sortedWinnersArray[0].penguin}, SILVER: ${sortedWinnersArray[1].penguin}, BRONZE: ${sortedWinnersArray[2].penguin}`;
+  }
+
+  function calculateTimeRemaining(lane) {
+    const laneArray = lane.split("");
+    let timeCounter = 0;
+
+    laneArray.forEach((waterTile) => {
+      if (waterTile === "-") {
+        timeCounter = timeCounter + 1;
+      }
+      if (waterTile === "~") {
+        timeCounter = timeCounter + 2;
+      }
+    });
+    return timeCounter;
+  }
 }
 
-//---------------------------------------------------------------------------------------
-// If you can't sleep, just count sheep!!
-var countSheep = function (num) {
-  //your code here
-  //       let count = i + 1;
-  let sheepCount = "";
-  //   if (num > 0){
-  for (let i = 0; i < num; i++) {
-    sheepCount += i + 1 + " sheep...";
+//-------------------------------------------------------------------------------------
+// A square of squares
+var isSquare = function (n) {
+  if (n === -1) {
+    return false;
   }
-  return sheepCount;
-  //   }
+  if (n === 0) {
+    return true;
+  }
+  let squareRoot = Math.sqrt(n);
+  console.log(n + " = " + squareRoot);
+
+  if (Number.isInteger(squareRoot)) {
+    if (squareRoot * squareRoot === n) {
+      return true;
+    }
+  }
+  return false; // fix me
 };
-//----------------------------------------------------------------------------------------
-// Multiplication table for number
-function multiTable(number) {
-  // good luck
-  let timesTable = "";
-  for (let i = 1; i <= 10; i++) {
-    //     console.log(number * i);
-    if (i === 10) {
-      timesTable += i + " * " + number + " = " + number * i;
-    } else {
-      timesTable += i + " * " + number + " = " + number * i + "\n";
-    }
-  }
-  return timesTable;
-}
-//----------------------------------------------------------------------------------------
-// Remove Exclamation
-return s.replace(/!/g, "");
-//----------------------------------------------------------------------------------------
-//Returning Strings
-function greet(name) {
-  return `Hello, ${name} how are you doing today?`;
-}
-//----------------------------------------------------------------------------------------
-//Sentence Smash
-// Smash Words
-function smash(words) {
-  //     "use strict";
-  let rToS = [...words].join(" ");
-  console.log(rToS);
-  return rToS;
-}
-//----------------------------------------------------------------------------------------
-//Training JS #16: Methods of String object--slice(), substring() and substr()
-function cutIt(arr) {
-  //coding here...
-  let shortestLength = Infinity;
 
-  for (let i = 0; i < arr.length; i++) {
-    //     console.log(arr[i]);
-    if (arr[i].length < shortestLength) {
-      shortestLength = arr[i].length;
-    }
-    console.log(shortestLength);
-  }
-  return arr.map((string) => string.slice(0, shortestLength));
-}
-//---------------------------------------------------------------------------------------
-// Penguin Olympics: Swimming Race Disaster
-
-function calculateWinners(snapshot, penguins) {
-  // TODO: solve kata
-  // RETURN "GOLD: <name-1>, SILVER: <name-2>, BRONZE: <name-3>"
-  // Top three penguins
-  // Snapshot is String of lanes
-  // Each has one Penguin
-  // Lanes are same length
-  // Penguis are names of penguins in order of lanes
-  // Penguins can be "p" or "P"
-  // Split snapshot into distance remaining.
-  // Count the value of the distance remaining in each lane.
-  // When counting "-" + 1 and "~" + 2
-  // Label penguins
-  // Order penguins with the least difficult route
-  // Return the winning panguins
-
-  const lanesArray = snapshot
-    .split("|")
-    .filter((lane) => lane.toLowerCase().includes("p"));
-  const remainingWaterArray = lanesArray.map((lane) => {
-    const penguinIndex = lane.toLowerCase().indexOf("p");
-    const remainingWater = lane.slice(penguinIndex + 1);
-    return remainingWater;
-  });
-  const remainingTimeArray = remainingWaterArray.map((lane) =>
-    calculateTimeRemaining(lane)
-  );
-  const penguinsAndTimesArray = remainingTimeArray.map((time, index) => {
-    return { time: time, penguin: penguins[index] };
-  });
-  const sortedWinnersArray = penguinsAndTimesArray.sort(
-    (a, b) => a.time - b.time
-  );
-  console.log(sortedWinnersArray);
-
-  return `GOLD: ${sortedWinnersArray[0].penguin}, SILVER: ${sortedWinnersArray[1].penguin}, BRONZE: ${sortedWinnersArray[2].penguin}`;
-}
-
-function calculateTimeRemaining(lane) {
-  const laneArray = lane.split("");
-  let timeCounter = 0;
-
-  laneArray.forEach((waterTile) => {
-    if (waterTile === "-") {
-      timeCounter = timeCounter + 1;
-    }
-    if (waterTile === "~") {
-      timeCounter = timeCounter + 2;
-    }
-  });
-  return timeCounter;
-}
-
-//--------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
